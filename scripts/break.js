2f75ef6cb148fd496eb1d88dc6ca7b21846101ff
leathan <1@leathan.xyz>
2017-07-11 20:23:35 -0400

    Added leet & break cmd. added auto brain backup to json

---
+++ scripts/break.js
@@ -0,0 +1,17 @@
+// Description:
+//   Sets a break point in the code allowing  live inspections and manipulations.
+//
+// Commands:
+//   hubot break|halt|freeze - halts the bot code.
+//
+
+module.exports = function(bot) {
+  bot.respond(/(freeze|halt|break)/i, {id: 'create.break'}, function(r) {
+    r.send("Completely halting for live callstack and direct code manipulation")
+    setTimeout(function(){
+      debugger;
+      r.send("Resuming normal functions.");
+    },1000)
+  })
+}
+
---
+++ scripts/leet.js
@@ -0,0 +1,19 @@
+// Description:
+//   Make hubot talk in leetspeak.
+//
+// Commands:
+//   hubot leet[ me] <text> - returns text in leetspeak.
+//
+// Author:
+//   leathan
+//
+
+(function(){
+const leet = require('1337');
+
+  module.exports = function(bot) {
+    bot.respond(/leet(?: me) (.*)/i, function(r) {
+      m.send(leet(msg.match[1]));
+    });
+  };
+}).call(this);
