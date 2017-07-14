31b05bd01a7edd5cea7a98ca5e7a5ee1e2b261e6
leathan <1@leathan.xyz>
2017-07-14 09:22:56 -0400

    Added propper ping command, added scan command

---
+++ scripts/check.js
@@ -0,0 +1,30 @@
+// Description:
+//   Check if a messae triggers a listener, and improve upon the default ping script.
+//
+// Commands:
+//   hubot check[ me] <text> - Checks if text is a command.
+//
+// Author:
+//   leathan
+//
+
+(function(){
+  module.exports = function(bot) {
+    bot.respond(/pong/i, function(r) {
+      r.send(bot.client.pings[0] + 'ms.');
+    });
+    bot.respond(/check(?: me) (.*)/i, function(r) {
+      r.send(scanMe(msg.match[1]));
+    });
+  };
+  scanMe = (string) => {
+    var res = [];
+    for (i=0; i<robot.listeners.length; i++) {
+      if (!robot.listeners[i].regex) continue;
+      if (robot.listeners[i].regex.test(string)) res.push(k)
+    }
+    return res;
+  }
+}).call(this);
+
+
