d9a75e4806926f4e7e621f037e5beccedcbf3315
leathan <1@leathan.xyz>
2017-07-18 13:52:03 -0400

    refactor, optimizations

--- scripts/mubot.js
+++ scripts/mubot.js
@@ -17,50 +17,53 @@
 // Author:
 //   Project Bitmark
 //
-
 (function() {
-  var adapter, deposit_marks, exec, from_URI, irc_server, last, marks, secret, slack_team, symbol, to_URI, transfer_marks, why_context, withdraw_marks;
+  var adapter, deposit_marks, exec, marks, symbol, why_context
   exec = require('child_process').exec;
   symbol = '₥';
-  last = 'Mubot';
-  why_context = '';
-
-  function to_URI(name) { return id }   // Pending future API creation
-  function from_URI(URI) { return URI } // ^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  function deposit_marks(msg, URI, amount, robot) { } // Will add when needed
-
-  function transfer_marks(msg, recipient, amount, robot, why_context) {
+  function to_URI(name) { return id }               // Pending future API
+  function from_URI(URI) { return URI }             // Pending future API
+  function deposit_marks(r, URI, amount, robot) { } // Will add when needed
+  function transfer_marks(r, recipient, amount, robot, why_context) {
     if (why_context == null) why_context = "N/A"
-    if (marks[msg.message.user.id] >= parseFloat(amount)) {
+    if (marks[r.message.user.id] >= parseFloat(amount)) {
       if (marks[recipient] == null) marks[recipient] = 0
       marks[recipient] += parseFloat(amount);
-      marks[msg.message.user.id] -= parseFloat(amount);
-      return msg.send(msg.message.user.name + ' has marked ' + robot.brain.userForId(recipient).name + ' ' + amount + symbol + '. ( ' + why_context + ' )');
+      marks[r.message.user.id] -= parseFloat(amount);
+      return r.send(r.message.user.name + ' has marked ' + robot.brain.userForId(recipient).name + ' ' + amount + symbol + '. ( ' + why_context + ' )');
     } else {
-      return msg.send('Sorry, but you dont have enough marks. Try the deposit command or get marked more.');
+      return r.send('Sorry, but you dont have enough marks. Try the deposit command or get marked more.');
     }
-  };
-
-  function withdraw_marks(msg, address, amount, robot) {
-    var command;
-    if (robot.brain.data.marks[to_URI(msg.message.user.name)] >= parseFloat(amount)) {
-      command = 'bitmark-cli sendtoaddress ' + address + ' ' + (parseFloat(amount) / 1000.0);
-      return exec(command, function(error, stdout, stderr) {
-        robot.brain.data.marks[to_URI(msg.message.user.name)] -= parseFloat(amount);
-        return msg.send(stdout);
+  }
+  function withdraw_marks(r, address, amount, robot) {
+    if (robot.brain.data.marks[r.message.user.id] >= parseFloat(amount)) {
+      var command = 'bitmark-cli sendtoaddress ' + address + ' ' + (parseFloat(amount) / 1000.0);
+      return exec(command, (error, stdout, stderr) => {
+        robot.brain.data.marks[r.message.user.id] -= parseFloat(amount);
+        return r.send(stdout);
       });
     } else {
-      return msg.send('Sorry, you have not been marked that many times yet.');
+      return r.send('Sorry, you have not been marked that many times yet.');
     }
-  };
-
+  }
   module.exports = robot => {
     adapter = robot.adapterName;
-    if (robot.brain.data.marks == null) robot.brain.data.marks = {}
-    marks = robot.brain.data.marks
-    if(marks['183771581829480448'] == null) marks['183771581829480448'] = 12000
-    if(marks['U02JGQLSQ'] == null) marks['U02JGQLSQ'] = 12000
-
+    robot.brain.on('loaded', ()=>{
+      //robot.brain.saveInterval(1) // (Removed): Logger should save on ever new msg.
+      if (robot.brain.data.marks == null) robot.brain.data.marks = {}; marks = robot.brain.data.marks;
+      if(marks['183771581829480448'] == null) marks['183771581829480448'] = 12000
+      if(marks['U02JGQLSQ']          == null) marks['U02JGQLSQ']          = 12000
+    })
+    robot.hear(/withdraw\s+([\w\S]+)\s+(\d+)\s*$/i, r => {
+      withdraw_marks(r, r.match[1], r.match[2], robot);
+    });
+    robot.hear(/marks\s*$/i, r => {
+      if (marks[r.message.user.id] == null) marks[r.message.user.id] = 0;
+      r.send('You have ' + marks[r.message.user.id] + symbol + '.');
+    });
+    robot.router.get("/api/marks", (req, res) => {
+      res.json(robot.brain.data.marks);
+    });
     if(adapter == 'discord') {
       robot.hear(/marks\s+@? (.*)#(\d{4})/i, r => {
         arr = robot.brain.usersForFuzzyName(r.match[1])
@@ -71,18 +74,18 @@
         } else if (arr.length < 1) { return r.send('User ' + r.match[1] + ' was not found.') }
         return r.send(r.match[1] + ' has 0' + symbol + '.')
       })
-      robot.hear(/marks\s+<@?!?(\d+)>$/i, function(msg) {
-       if (robot.brain.data.users[msg.match[1]]) {
-          if (marks[msg.match[1]]  == null) marks[msg.match[1]] = 0
-          return msg.send(robot.brain.data.users[msg.match[1]] + ' has ' + marks[msg.match[1]] + symbol + '.');
+      robot.hear(/marks\s+<@?!?(\d+)>$/i, r => {
+       if (robot.brain.data.users[r.match[1]]) {
+          if (marks[r.match[1]]  == null) marks[r.match[1]] = 0
+          return r.send(robot.brain.data.users[r.match[1]] + ' has ' + marks[r.match[1]] + symbol + '.');
         } else {
-          return msg.send("Sorry, I can't find that user.");
+          return r.send("Sorry, I can't find that user.");
         }
       })
-      robot.hear(/^\+(\d+)\s+<@?!?(\d+)>\s*(.*)?$/i, msg => {
-        if (msg.match[2] === robot.client.user)   return msg.send("Sorry but I am currently unmarkable.");
-        if (msg.match[2] === msg.message.user.id) return msg.send("Sorry but you cannot mark yourself.");
-        if (msg.match[1] <= 100) { return transfer_marks(msg, msg.match[2], msg.match[1], robot, msg.match[3]) } else { return msg.send('Max is +100') }
+      robot.hear(/^\+(\d+)\s+<@?!?(\d+)>\s*(.*)?$/i, r => {
+        if (r.match[2] === robot.client.user)   return r.send("Sorry but I am currently unmarkable.");
+        if (r.match[2] === r.message.user.id) return r.send("Sorry but you cannot mark yourself.");
+        if (r.match[1] <= 100) { return transfer_marks(r, r.match[2], r.match[1], robot, r.match[3]) } else { return r.send('Max is +100') }
       })
       robot.hear(/\+(\d+)\s+@ (.*)#(\d{4}) ?(.*)/i, r => {
         var rec; var arr = robot.brain.usersForFuzzyName(r.match[2])
@@ -92,41 +95,28 @@
         else if (arr.length < 1) { return r.send('User ' + r.match[2] + ' was not found.') }
         if ((r.match[2].toLowerCase() == robot.name.toLowerCase()) && r.match[3] == robot.client.user.discriminator) return r.send("Sorry but I am currently unmarkable.");
         if (rec === r.message.user.id) return r.send("Sorry but you cannot mark yourself.");
-        if (r.match[1] <= 100) { return transfer_marks(r, rec, r.match[1], robot, r.match[4]) } else { return r.send('Max is +100') }
+        if (r.match[1] <= 100) transfer_marks(r, rec, r.match[1], robot, r.match[4]); else r.send('Max is +100')
       })
-    } else {
-      robot.hear(/marks\s*@? ?(\w+)$/i, function(msg) {
-        if (robot.brain.userForName(msg.match[1]) == null) return msg.send("Sorry but I cant find that user.");
-        if (marks[robot.brain.userForName(msg.match[1])] == null) marks[robot.brain.userForName(msg.match[1])] = 0
-        msg.send(msg.match[1] + ' has ' + marks[robot.brain.userForName(msg.match[1]).id] + symbol + '.');
-      })
-      robot.respond(/\+(\d+)\s+@? ?(\w+)\s*(.*)?$/i, function(msg) {
-        if (robot.brain.userForName(msg.match[2]) == null) return msg.send("Sorry but I cant find that user.");
-        if (robot.brain.userForName(msg.match[2]).id == robot.adapter.self.id)  return msg.send("Sorry but I am currently unmarkable.");
-        if (robot.brain.userForName(msg.match[2]).id == msg.message.user.id) return msg.send("Sorry but you cannot mark yourself.");
-        if (msg.match[1] <= 100) { return transfer_marks(msg, robot.brain.userForName(msg.match[2]).id, msg.match[1], robot, msg.match[3]) } else { return msg.send('Max is +100') }
-      })
-    }
-    robot.hear(/withdraw\s+([\w\S]+)\s+(\d+)\s*$/i, function(msg) {
-      return withdraw_marks(msg, msg.match[1], msg.match[2], robot);
-    });
-    robot.hear(/marks\s*$/i, function(msg) {
-      if (marks[msg.message.user.id] == null) marks[msg.message.user.id] = 0;
-      msg.send('You have ' + marks[msg.message.user.id] + symbol + '.');
-    });
-    if(adapter == 'slack') {
-      robot.react((r)=>{
+    } else if(adapter == 'slack') {
+      robot.react(r => {
         if(r.message.type==='added' && r.message.reaction==='mh') {
           var senderID=r.message.user.id; receiverID=r.message.item_user.id
           transfer_marks(r, r.message.item_user.id, 1, robot, "reaction")
         }
       })
-    } else if(adapter == 'discord') {
-      // To be continued ...
+      robot.hear(/marks\s*@? ?(\w+)$/i, r => {
+        var user = robot.brain.userForName(r.match[1]);
+        if (user == null) return r.send("Sorry but I cant find that user.");
+        if (marks[user.id] == null) marks[user.id] = 0
+        r.send(r.match[1] + ' has ' + marks[user.id] + symbol + '.');
+      })
+      robot.hear(/\+(\d+)\s+@? ?(\w+)\s*(.*)?$/i, r => {
+        var rec = robot.brain.userForName(r.match[2])
+        if (  rec  ==  null  )                return r.send("Sorry but I cant find that user.");
+        if (rec.id == robot.adapter.self.id)  return r.send("Sorry but I am currently unmarkable.");
+        if (rec.id == r.message.user.id)      return r.send("Sorry but you cannot mark yourself.");
+        if (r.match[1] <= 100) transfer_marks(r, rec.id, r.match[1], robot, r.match[3]); else return r.send('Max is +100')
+      })
     }
-    return robot.router.get("/api/marks", function(req, res) {
-      return res.json(robot.brain.data.marks);
-    });
   };
-
 }).call(this);
