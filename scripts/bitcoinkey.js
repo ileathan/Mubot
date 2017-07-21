ff033c0ba90467f2cc857a28233ecd0c886fe73f
leathan <1@leathan.xyz>
2017-07-20 20:34:05 -0400

    bitcoin me command (returns private key)

---
+++ scripts/bitcoinkey.js
@@ -0,0 +1,9 @@
+// Requisites (should be preinstalled)
+var crypto = require('crypto');
+// Pulled directly from the secp256k1 ECDSA specification on private key lengths.
+var MIN = new Buffer('FFF0000000000000000000000000000000000000000000000000000000000001', 'hex');
+var MAX = new Buffer('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140', 'hex');
+
+x = _ => !(_.compare(MIN) < 0) && !(_.compare(MAX) > 0)?_:x(crypto.randomBytes(32))
+
+module.exports = b => b.respond(/btc(?: key)? me$/i, r => r.send(x(crypto.randomBytes(32)).toString('hex')))
