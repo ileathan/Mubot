2c80cbbdec81eb249b3530c3a25359c709663df6
Spinelli Valentinuzzi <1@Mubot.com>
2017-07-24 05:44:09 -0400

    cleaned up namespace

--- multisig_example.js
+++ multisig_example.js
@@ -1,14 +1,12 @@
-pubkey1='02e20a05e17c62c861130acde75175143ae5586e80925a72a6ff1394cb25b23d3a'
-pubkey2='021aa8bcd62e7210167b908367a9a67bc652ec4c9f3c849a76387c512c23eb105e'
-
+var exec, pubkey1, pubkey2, res, address, redeemScript
 exec = require('child_process').execSync
 
+pubkey1='02e20a05e17c62c861130acde75175143ae5586e80925a72a6ff1394cb25b23d3a'
+pubkey2='021aa8bcd62e7210167b908367a9a67bc652ec4c9f3c849a76387c512c23eb105e'
 
 res = JSON.parse(exec(`bitmarkd createmultisig 2 '["${pubkey1}", "${pubkey2}"]'`).toString())
 
 address = res.address;
 redeemScript = res.redeemScript;
-
-
 
 console.log([address, redeemScript])
