6819f689f197295033a8dfd036330c1033d827db
Spinelli Valentinuzzi <1@Mubot.com>
2017-07-25 10:44:13 -0400

    Update zzzdbwrite.js

--- scripts/zzzdbwrite.js
+++ scripts/zzzdbwrite.js
@@ -6,10 +6,9 @@
 (function() {
   var fs = require('fs'), path = require('path');
   module.exports = function(robot) {
-    var data;
     robot.brain.setAutoSave(false);
     try {
-      data = fs.readFileSync(path.join(__dirname, '/../brain.json') 'utf-8');
+      var data = fs.readFileSync(path.join(__dirname, '/../brain.json') 'utf-8');
       if (data) robot.brain.mergeData(JSON.parse(data));
     } catch (err) { if (err.code !== 'ENOENT') console.log(error); }
     return robot.brain.on('save', function(data) {
