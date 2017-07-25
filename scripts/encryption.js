7a40979518a6d4a5c657ae49a30c9ede52146329
leathan <1@leathan.xyz>
2017-07-25 11:29:12 -0400

    bugfix

--- scripts/encryption.js
+++ scripts/encryption.js
@@ -9,9 +9,7 @@
     })
     bot.respond(/encrypt (<@?!?(\d+)>) (.*)/i, res => {
       if(!keys[res.message.user.id]) return res.send("Sorry, you need a base keypair first.")
-      receiver = bot.brain.userForId(res.match[2]);
-      if(!receiver) return res.send("Sorry, I cant find that user.")
-      if(!keys[receiver]) return res.send("Sorry, but your recipient needs a base keypair.")
+      if(!keys[msg.match[2]]) return res.send("Sorry, but your recipient needs a base keypair.")
       eccrypto.encrypt(keys[receiver].public, Buffer(msg.match[3])).then(encrypted => res.send(res.match[1] + " " + encrypted))
     })
 
