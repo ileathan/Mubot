3b710616e638282b84a57406b47ae474b92218b8
leathan <1@leathan.xyz>
2017-07-26 05:34:05 -0400

    refactors

--- scripts/zzzdbwrite.js
+++ scripts/zzzdbwrite.js
@@ -1,6 +1,10 @@
 // Description:
 //   Removes auto save from brain, and writes to disk on save event.
 //
+// Configureation:
+//   set robot.brain.setAutoSave(false) to true, for more writes.
+//   currently its set to save just when user balances change.
+//
 // Author:
 //   leathan
 (function() {
@@ -12,8 +16,10 @@
       var data = fs.readFileSync(path, 'utf-8');
       if (data) robot.brain.mergeData(JSON.parse(data));
     } catch (err) { if (err.code !== 'ENOENT') console.log(err); }
-    return robot.brain.on('save', function(data) {
-      fs.writeFile(path, JSON.stringify(data), 'utf-8', ()=>{});
-    });
+    robot.brain.on('save', write)
+    robot.brain.on('close', write)
+    robot.brain.on('shutdown', write)
+    robot.brain.on('shutdown', write)
+    const write =  data => fs.writeFile(path, JSON.stringify(data), 'utf-8', ()=>{});
   };
 }).call(this);
