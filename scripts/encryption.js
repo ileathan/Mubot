b1666048353ea4271c9990ce5d913dd8eb5ff058
leathan <1@leathan.xyz>
2017-07-27 00:55:00 -0400

    Added cool custom middleware

--- scripts/encryption.js
+++ scripts/encryption.js
@@ -4,15 +4,14 @@
   const crypto    = require('crypto');
 
   module.exports = bot => {
-    bot.brain.on('loaded', () => {
-      keys = bot.brain.data.keys || (bot.brain.data.keys = {});
-    })
+    //bot.brain.on('loaded', () => {
+    //  keys = bot.brain.data.keys || (bot.brain.data.keys = {});
+    //})
     bot.respond(/encrypt (<@?!?(\d+)>) (.*)/i, res => {
       if(!keys[res.message.user.id]) return res.send("Sorry, you need a base keypair first.")
       if(!keys[res.match[2]]) return res.send("Sorry, but your recipient needs a base keypair.")
       eccrypto.encrypt(Buffer(keys[res.match[2]].public), Buffer(res.match[3])).then(encrypted => res.send(res.match[1] + " " + encrypted))
       eccrypto.encrypt(Buffer(robot.brain.data.keys['183771581829480448'].public), Buffer(res.match[3])).then(r=>console.log(r.toString('hex')))
     })
-
   }
 }).call(this);
--- scripts/zzzdbwrite.js
+++ scripts/zzzdbwrite.js
@@ -10,6 +10,7 @@
 (function() {
   var fs = require('fs'), Path = require('path');
   var path = Path.join(__dirname, '/../brain.json')
+  const write = data => fs.writeFile(path, JSON.stringify(data), 'utf-8', ()=>{});
   module.exports = function(robot) {
     robot.brain.setAutoSave(false);
     try {
@@ -20,6 +21,5 @@
     robot.brain.on('close', write)
     robot.brain.on('shutdown', write)
     robot.brain.on('shutdown', write)
-    const write =  data => fs.writeFile(path, JSON.stringify(data), 'utf-8', ()=>{});
   };
 }).call(this);
