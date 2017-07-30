cbbac50bd84c20519dc15e2fd9f920b7a4ffcae4
leathan <1@leathan.xyz>
2017-07-30 09:44:51 -0400

    removed debugging lives

--- scripts/google.js
+++ scripts/google.js
@@ -2,23 +2,30 @@
 //   A way to interact with the Google Web API.
 //
 // Commands:
-//   hubot google|g <query> - Queries Google Web for <query> and returns the first result.
-
+//   mubot google|g <query> <index> - Queries google and returns result number <index>.
+//   mubot image[ me] <query> - Queries google images and returns result number <index>.
+//
 var cheerio = require('cheerio')
-var scrape  = require('scrapeit')
 var request = require('request')
 
 module.exports = function (robot) {
   robot.respond(/(?:g|google) (?:me )?(.*)/i, function (msg) {
-    try { index = msg.match[1].match(/([1-9]|1[1-8])$/)[1] } catch(e) { index = "" }
+    try
+      index = msg.match[1].match(/([1-9]|1[1-8])$/)[1]
+     } catch(e) {
+        index = ""
+     }
     query = msg.match[1].slice(0, msg.match[1].length - index.toString().length)
     googleMe(msg, index, query, function(url) {
       msg.send(url)
     })
   })
   robot.respond(/(?:image) (?:me )?(.*)/i, function (msg) {
-    try { index = msg.match[1].match(/([1-9]|1[1-8])$/)[1] } catch(e) { index = "" }
-    console.log(index)
+   try {
+     index = msg.match[1].match(/([1-9]|1[1-8])$/)[1]
+    } catch(e) {
+      index = ""
+    }
     query = msg.match[1].slice(0, msg.match[1].length - index.toString().length)
     googleImgMe(robot, index, query, function(url) {
       msg.send(url)
@@ -27,18 +34,14 @@ module.exports = function (robot) {
 }
 function googleImgMe(robot, index, query, cb) {
   if (index == "") index = 1
-  request("https://www.google.com/search?tbm=isch&q="+encodeURIComponent(query), function(err, res, html){ 
+  request("https://www.google.com/search?tbm=isch&q="+encodeURIComponent(query), (err, res, html) => {
     c = cheerio.load(html)('div img')[+index+1].parent.attribs.href
     cb(decodeURIComponent(c.slice( 7, c.indexOf('&'))))
   })
 }
 function googleMe(msg, index, query, cb) {
   if (index == "") index = 1
-  request("https://www.google.com/search?q="+encodeURIComponent(query),  function(e, r, html) {
-    console.log(cheerio.load(html)('.r a')[index-1].attribs.href)
-    console.log(cheerio.load(html)('.r a')[index].attribs.href)
-    console.log(cheerio.load(html)('.r a')[index+1].attribs.href)
-    console.log(cheerio.load(html)('.r a')[index+2].attribs.href)
+  request("https://www.google.com/search?q="+encodeURIComponent(query), (e, r, html) => {
     o = cheerio.load(html)('.r a')[index-1].attribs.href
     if (/^\/search\?/.test(o)) o = cheerio.load(html)('.r a')[index].attribs.href
     cb(decodeURIComponent(o.slice(7, o.indexOf('&'))))
