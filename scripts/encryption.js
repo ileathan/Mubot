190140483fa965d86f824f669b26bd006a0ab021
leathan <1@leathan.xyz>
2017-08-02 00:26:26 -0400

    new docs

--- scripts/encryption.js
+++ scripts/encryption.js
@@ -1,12 +1,19 @@
+// Description:
+//   Encrypts a message to a users using their public key.
+//
+// Commands:
+//   mubot encryt Alice hello - Encrypts the message hello using Alice's pubkey.
+//
+
 (function() {
   const secp256k1 = require('secp256k1');
   const eccrypto  = require('eccrypto');
   const crypto    = require('crypto');
 
   module.exports = bot => {
-    //bot.brain.on('loaded', () => {
-    //  keys = bot.brain.data.keys || (bot.brain.data.keys = {});
-    //})
+    bot.brain.on('loaded', () => {
+      keys = bot.brain.data.keys || (bot.brain.data.keys = {});
+    })
     bot.respond(/encrypt (<@?!?(\d+)>) (.*)/i, res => {
       if(!keys[res.message.user.id]) return res.send("Sorry, you need a base keypair first.")
       if(!keys[res.match[2]]) return res.send("Sorry, but your recipient needs a base keypair.")
