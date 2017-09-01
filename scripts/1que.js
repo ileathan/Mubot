e306a1e3998731e0eb2fb6d9e57e6fb692be5501
leathan <1@leathan.xyz>
2017-09-01 12:16:14 -0400

    Refactor

--- scripts/1que.js
+++ scripts/1que.js
@@ -1,49 +1,57 @@
 // Description:
 //   Allows sending messages over the max length for discord, also enforces power commands.
 //
-
 (function() {
-  var POWER_COMMANDS = ['create.file', 'view.file', 'create.break', 'create.die'],
-      POWER_USERS    = ['183771581829480448', 'U02JGQLSQ'];
-
+  var POWER_COMMANDS = ['create.file', 'view.file', 'create.break', 'create.die'], POWER_USERS = ['183771581829480448', 'U02JGQLSQ'];
   module.exports = function(robot) {
     var adapter = robot.adapterName;
     robot.listenerMiddleware((context, next, done) => {
-      if (POWER_COMMANDS.indexOf(context.listener.options.id) >= 0) {
-        if (POWER_USERS.indexOf(context.response.message.user.id) >= 0) return next();
+      if(POWER_COMMANDS.includes(context.listener.options.id)) {
+        if(POWER_USERS.includes(context.response.message.user.id)) return next();
         else {
           context.response.send("I'm sorry, @" + context.response.message.user.name + ", but you don't have access to do that.");
-          return done();
+          return done()
         }
-      } else return next();
+      } else return next()
     });
     robot.responseMiddleware((context, next, done) => {
       if (!context.plaintext || !context.strings[0] || !context.strings[0].length) return done();
       function chunkAndQue(i) { // i is our iterator.
-        var epad = fpad = adapter === 'discord' ? "**" : "*", m; // m is our chunk
-        if (context.strings[i] && context.strings[i].length > 2000) { // only proceed if we need to break msg down to chunks.
-          if (context.response.match[0].indexOf('view') === 0) {
-            fpad = '```javascript\n'; // The command is a view code command
-            epad = '```';             // so pad it with code markdown.
+        // Pad the start of the message, and the end of the message.
+        var epad = fpad = adapter === 'discord' ? "**" : "*";
+        // m is our chunk
+        var m;
+        // only proceed if we need to break msg down to chunks.
+        if(context.strings[i] && context.strings[i].length > 2000) {
+          if(context.response.match[0].indexOf('view') === 0) {
+            // The command is a view code command
+            fpad = '```javascript\n';
+            // so pad it with code markdown.
+            epad = '```'
           }
-          if (context.response.match[0].indexOf('search') === 0) {
-            fpad = '```\n';  // The command is a search web command
-            epad = '```';    // so pad it with code markdown.
+          if(context.response.match[0].indexOf('search') === 0) {
+            // The command is a search web command
+            fpad = '```\n';
+            // so pad it with code markdown.
+            epad = '```'
           }
-          m = context.strings[i].match(/^([\s\S]{0,1940}\n)/); // Try to get biggest chunk possible until newline char.
-          if (!(m ? m[0] : void 0)) {
-            m = context.strings[i].match(/^([\s\S]{0,1940})/); // There was no newline, fallback to biggest chunk.
+          // Try to get biggest chunk possible until newline char.
+          m = context.strings[i].match(/^([\s\S]{0,1940}\n)/);
+          if(!(m ? m[0] : void 0)) {
+            // There was no newline, fallback to biggest chunk.
+            m = context.strings[i].match(/^([\s\S]{0,1940})/)
           }
-          context.strings.push("" + fpad + context.strings[i].slice(m[1].length)); // Apply the pad to large chunk, and que it.
-          context.strings[i] = context.strings[i].slice(0, m[1].length) + epad; // This is the garanteed small chunk. (first out of que)
-          i++;
-          return chunkAndQue(i);
+          // Apply the pad to large chunk, and que it.
+          context.strings.push("" + fpad + context.strings[i].slice(m[1].length));
+          // This is the garanteed small chunk. (first out of que)
+          context.strings[i] = context.strings[i].slice(0, m[1].length) + epad;
+          return chunkAndQue(++i)
         } else {
-          context.strings[0] = fpad + context.strings[0] + epad;
+          context.strings[0] = fpad + context.strings[0] + epad
         }
       }
       chunkAndQue(0);
-      return next();
+      return next()
     })
-  };
+  }
 }).call(this);
