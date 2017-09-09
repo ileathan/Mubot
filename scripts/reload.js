ef6f446d5afb9f4e4bb9a88f70faa1f42babcef9
leathan <1@leathan.xyz>
2017-09-09 08:31:37 -0400

    refactor

--- scripts/reload.js
+++ scripts/reload.js
@@ -2,7 +2,7 @@
 //   Allows Hubot to (re)load scripts without restart
 //
 // Commands:
-//   hubot reload all scripts - Reloads scripts without restart. Loads new scripts too.
+//   hubot reload - Reloads scripts without restart. Loads new scripts too.
 //   hubot command count - Tells how many commands hubot knows
 //
 // Author:
@@ -14,33 +14,34 @@
   Path = require('path');
   oldCommands = oldListeners = null;
 
-  module.exports = function(robot) {
-    robot.respond(/.*command count.*/i, function(msg) {
-      msg.send("I am aware of " + msg.robot.commands.length + " commands.");
+  module.exports = bot => {
+    bot.respond(/.*command count.*/i, msg => {
+      msg.send("I am aware of " + msg.bot.commands.length + " commands.")
     });
-    robot.respond(/reload$/i, function(msg) {
-      oldCommands = robot.commands;
-      oldListeners = robot.listeners;
-      robot.commands = [];
-      robot.listeners = [];
-      reloadAllScripts(msg);
-    });
+    bot.respond(/reload$/i, msg => {
+      oldCommands = bot.commands;
+      oldListeners = bot.listeners;
+      bot.commands = [];
+      bot.listeners = [];
+      reloadAllScripts(msg)
+    })
   };
-  reloadAllScripts = function(msg) {
-    var scripts, robot = msg.robot;
-    robot.events._events = {}
-    try {
-      robot.middleware.listener.stack = []
-      robot.middleware.response.stack = []
-      robot.middleware.receiver.stack = []
-    } catch(e) {}
-    robot.load(Path.resolve(".", "scripts"));
-    robot.load(Path.resolve(".", "src", "scripts"));
+  reloadAllScripts = msg => {
+    var scripts, bot;
+    bot = msg.bot;
+    bot.events._events = {}
+    bot.middleware.listener.stack = [];
+    bot.middleware.response.stack = [];
+    bot.middleware.receiver.stack = [];
+    bot.load(Path.resolve(".", "scripts"));
+    bot.load(Path.resolve(".", "src", "scripts"));
     try {
       Fs.readFile(Path.resolve(".", "external-scripts.json"), (err, res) => {
-        robot.loadExternalScripts(JSON.parse(res))
+        bot.loadExternalScripts(JSON.parse(res))
       })
-    } catch(e) { msg.send("Error loading external-scripts.json " + e) }
-    msg.send("Reloaded all scripts.");
-  };
+    } catch(e) {
+      msg.send("Error loading external-scripts.json " + e)
+    }
+    msg.send("Reloaded all scripts.")
+  }
 }).call(this);
