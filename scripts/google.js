06b2a06986e108f290781060b117a0229ea1152c
leathan <1@leathan.xyz>
2017-09-13 09:34:13 -0400

    fixed globals in google script

--- scripts/google.js
+++ scripts/google.js
@@ -5,41 +5,34 @@
 //   mubot google|g <query> <index> - Queries google and returns result number <index>.
 //   mubot image[ me] <query> - Queries google images and returns result number <index>.
 //
-var cheerio = require('cheerio')
-var request = require('request')
+(function(){
+  const cheerio = require('cheerio');
+  const request = require('request');
 
-module.exports = function (robot) {
-  robot.respond(/(?:g|google) (?:me )?(.*)/i, function (msg) {
-    try { var index = msg.match(/([1-9]|1[1-8])$/)[1] } catch(e) { var index = "" }
-    query = msg.match[1].slice(0, msg.match[1].length - index.toString().length)
-    googleMe(msg, index, query, function(url) {
-      msg.send(url)
+  module.exports = bot => {
+    bot.respond(/(g|google|image) (?:me )?(.*)/i, msg => {
+      var index, query;
+      index = msg.match[2].match(/[1-9]|1[1-8]$/) && index = index[1];
+      query = msg.match[2].slice(0, -index.length);
+      msg.match[1] === 'image' ?
+        googleImgMe(bot, index, query, url => msg.send(url))
+      :
+        googleMe(msg, index, query, url => msg.send(url))
     })
-  })
-  robot.respond(/(?:image) (?:me )?(.*)/i, function (msg) {
-   try {
-     index = msg.match[1].match(/([1-9]|1[1-8])$/)[1]
-    } catch(e) {
-      index = ""
-    }
-    query = msg.match[1].slice(0, msg.match[1].length - index.toString().length)
-    googleImgMe(robot, index, query, function(url) {
-      msg.send(url)
+  }
+  function googleImgMe(bot, index, query, cb) {
+    !index && (index = 1);
+    request("https://www.google.com/search?tbm=isch&q="+encodeURIComponent(query), (err, res, html) => {
+      var ref = cheerio.load(html)('div img')[+index + 1].parent.attribs.href;
+      cb(decodeURIComponent(ref.slice( 7, ref.indexOf('&'))))
     })
-  })
-}
-function googleImgMe(robot, index, query, cb) {
-  if (index == "") index = 1
-  request("https://www.google.com/search?tbm=isch&q="+encodeURIComponent(query), (err, res, html) => {
-    c = cheerio.load(html)('div img')[+index+1].parent.attribs.href
-    cb(decodeURIComponent(c.slice( 7, c.indexOf('&'))))
-  })
-}
-function googleMe(msg, index, query, cb) {
-  if (index == "") index = 1
-  request("https://www.google.com/search?q="+encodeURIComponent(query), (e, r, html) => {
-    o = cheerio.load(html)('.r a')[index-1].attribs.href
-    if (/^\/search\?/.test(o)) o = cheerio.load(html)('.r a')[index].attribs.href
-    cb(decodeURIComponent(o.slice(7, o.indexOf('&'))))
-  })
-}
+  }
+  function googleMe(msg, index, query, cb) {
+    !index && (index = 1);
+    request("https://www.google.com/search?q="+encodeURIComponent(query), (err, res, html) => {
+      var ref = cheerio.load(html)('.r a')[index-1].attribs.href;
+      if(/^\/search\?/.test(ref)) ref = cheerio.load(html)('.r a')[index].attribs.href;
+      cb(decodeURIComponent(ref.slice(7, ref.indexOf('&'))))
+    })
+  }
+}).call(this);
