242bd713fa2ecf0e0e9c2726bda1d11b88667f94
leathan <1@leathan.xyz>
2017-09-16 18:00:43 -0400

    fixes

--- scripts/availability.js
+++ scripts/availability.js
@@ -14,70 +14,62 @@
 //   hubot i am <unavailable|dnd|do not disturb|busy|in the zone|away|gone|afk|brb> - Setthat you are not available - (the 'i am' is optional)
 //
 // Author:
-//  tombell or leathan
+//  tombell
+//  leathan
 
 (function() {
-  module.exports = function(robot) {
-    var findUser;
-    findUser = function(name) {
-      var users;
-      users = robot.brain.usersForFuzzyName(name);
-      if (users.length === 1) {
-        return users[0];
-      } else if (users.length > 1) {
-        return users;
+  module.exports = bot => {
+    function findUser function(name) {
+      var users = bot.brain.usersForFuzzyName(name);
+      if(users.length === 1) {
+        return users[0]
+      } else if(users.length > 1) {
+        return users
       } else {
-        return false;
+        return false
       }
-    };
-    robot.respond(/is (.*) available(\?)?/i, function(msg) {
+    }
+    bot.respond(/is (.*) available(\?)?/i, msg => {
       var name, user;
       name = msg.match[1];
       user = findUser(name);
-      if (typeof user === 'object') {
-        if (user.available.available) {
-          return msg.send(user.name + " has been available since " + user.available.timestamp);
+      if(user && !user.length) {
+        if(user.available.available) {
+          msg.send(user.name + " has been available since " + user.available.timestamp)
         } else {
-          return msg.send(user.name + " has been unavailable since " + user.available.timestamp);
+          msg.send(user.name + " has been unavailable since " + user.available.timestamp)
         }
-      } else if (typeof user.length > 1) {
-        return msg.send("I found " + user.length + " people named " + name);
+      } else if(user.length) {
+        msg.send("I found " + user.length + " people named " + name)
       } else {
-        return msg.send("I have never met " + name);
+        msg.send("I have never met " + name)
       }
     });
-    robot.respond(/((i am ))?(available|free|not busy|at hand|back|here)/i, function(msg) {
+    bot.respond(/(?:i am )?(?:available|free|not busy|at hand|back|here)/i, msg => {
       var name, user;
       name = msg.message.user.name;
       user = findUser(name);
-      if (typeof user === 'object') {
-        user.available = {
-          available: true,
-          timestamp: new Date
-        };
-        return msg.reply("Okay, I have set you as available");
-      } else if (typeof user.length > 1) {
-        return msg.send("I found " + user.length + " people named " + name);
+      if(user && !user.length) {
+        user.available = { available: true, timestamp: new Date };
+        msg.reply("Okay, I have set you as available");
+      } else if(user.length) {
+        msg.send("I found " + user.length + " people named " + name)
       } else {
-        return msg.send("I have never met " + name);
+        msg.send("I have never met " + name)
       }
     });
-    return robot.respond(/((i am ))? (unavailable|dnd|do not disturb|busy|in the zone|away|gone|afk|brb)/i, function(msg) {
+    bot.respond(/(?:i am )?(?:unavailable|dnd|do not disturb|busy|in the zone|away|gone|afk|brb)/i, msg => {
       var name, user;
       name = msg.message.user.name;
       user = findUser(name);
-      if (typeof user === 'object') {
-        user.available = {
-          available: false,
-          timestamp: new Date
-        };
-        return msg.reply("Okay, I have set you as unavailable");
-      } else if (typeof user.length > 1) {
-        return msg.send("I found " + user.length + " people named " + name);
+      if(user && !user.length) {
+        user.available = { available: false, timestamp: new Date };
+        msg.reply("Okay, I have set you as unavailable")
+      } else if(user.length) {
+        msg.send("I found " + user.length + " people named " + name)
       } else {
-        return msg.send("I have never met " + name);
+        msg.send("I have never met " + name)
       }
-    });
-  };
-
+    })
+  }
 }).call(this);
