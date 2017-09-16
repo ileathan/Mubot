42a16453be5119f19e2ed74776df853188301c51
leathan <1@leathan.xyz>
2017-09-15 22:19:34 -0400

    removed debug command

--- scripts/encryption.js
+++ scripts/encryption.js
@@ -2,7 +2,7 @@
 //   Encrypts a message to a users using their public key.
 //
 // Commands:
-//   mubot encryt Alice hello - Encrypts the message hello using Alice's pubkey.
+//   mubot encrypt Alice hello - Encrypts the message hello using Alice's pubkey.
 //
 
 (function() {
@@ -15,10 +15,9 @@
       keys = bot.brain.data.keys || (bot.brain.data.keys = {});
     })
     bot.respond(/encrypt (<@?!?(\d+)>) (.*)/i, res => {
-      if(!keys[res.message.user.id]) return res.send("Sorry, you need a base keypair first.")
-      if(!keys[res.match[2]]) return res.send("Sorry, but your recipient needs a base keypair.")
+      if(!keys[res.message.user.id]) return res.send("Sorry, you need a base keypair first.");
+      if(!keys[res.match[2]]) return res.send("Sorry, but your recipient needs a base keypair.");
       eccrypto.encrypt(Buffer(keys[res.match[2]].public), Buffer(res.match[3])).then(encrypted => res.send(res.match[1] + " " + encrypted))
-      eccrypto.encrypt(Buffer(robot.brain.data.keys['183771581829480448'].public), Buffer(res.match[3])).then(r=>console.log(r.toString('hex')))
     })
   }
 }).call(this);
