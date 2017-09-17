db95a7f123d85b97cb78a88a24da0608c90d1123
leathan <1@leathan.xyz>
2017-09-17 18:08:56 -0400

    refactor

--- scripts/bitly.js
+++ scripts/bitly.js
@@ -12,43 +12,39 @@
 //   http://bit.ly/[hash] - looks up the real url
 //
 // Author:
+//   leathan
 //   sleekslush
 //   drdamour
 //   johnwyles
 
 (function() {
-  module.exports = function(robot) {
-    robot.respond(/(bitly|shorten)\s?(me)?\s?(.+)$/i, function(msg) {
+  module.exports = bot => {
+    bot.respond(/(?:bitly|shorten)(?: me)? (.+)$/i, msg => {
       return msg.http("https://api-ssl.bitly.com/v3/shorten").query({
         access_token: process.env.HUBOT_BITLY_ACCESS_TOKEN,
-        longUrl: msg.match[3],
+        longUrl: msg.match[1],
         format: "json"
       }).get()(function(err, res, body) {
         var response;
         response = JSON.parse(body);
-        return msg.send(response.status_code === 200 ? response.data.url : response.status_txt);
-      });
+        msg.send(response.status_code === 200 ? response.data.url : response.status_txt)
+      })
     });
-    return robot.hear(/(https?:\/\/(bit\.ly|yhoo\.it|j\.mp|pep\.si|amzn\.to)\/[0-9a-z\-]+)/ig, function(msg) {
-      return msg.http("https://api-ssl.bitly.com/v3/expand").query({
+    bot.hear(/(?:https?:\/\/(bit\.ly|yhoo\.it|j\.mp|pep\.si|amzn\.to)\/[0-9a-z\-]+)/ig, msg => {
+      msg.http("https://api-ssl.bitly.com/v3/expand").query({
         access_token: process.env.HUBOT_BITLY_ACCESS_TOKEN,
         shortUrl: msg.match
-      }).get()(function(err, res, body) {
-        var i, len, m, parsedBody, ref, results;
-        parsedBody = JSON.parse(body);
-        if (parsedBody.status_code === !200) {
-          msg.send("Lookup failed " + response.status_txt);
-          return;
+      }).get()((err, res, body) => {
+        var ref, parsedBody = JSON.parse(body);
+        if(parsedBody.status_code === !200) {
+          return msg.send("Lookup failed " + response.status_txt)
         }
         ref = parsedBody.data.expand;
-        results = [];
-        for (i = 0, len = ref.length; i < len; i++) {
-          m = ref[i];
-          results.push(msg.send(m.short_url + " is " + m.long_url));
+        for(let i = 0, len = ref.length; i < len; ++i) {
+          ref = ref[i];
+          msg.send(ref.short_url + " is " + ref.long_url)
         }
-        return results;
-      });
-    });
-  };
-
+      })
+    })
+  }
 }).call(this);
