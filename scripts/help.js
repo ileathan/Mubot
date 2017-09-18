4de1386200017b523173a1cad61733a05dd66647
leathan <1@leathan.xyz>
2017-09-18 13:34:44 -0400

    bugfix/refactos

--- scripts/help.js
+++ scripts/help.js
@@ -18,33 +18,26 @@
 //
 
 (function() {
-  var getHelpCommands, helpContents, hiddenCommandsPattern;
-
-  helpContents = (name, commands) =>
-     "<!DOCTYPE html>\n<html>\n  <head>\n  <meta charset=\"utf-8\">\n  <title>" + name + " Help</title>\n  <style type=\"text/css\">\n    body {\n      background: #d3d6d9;\n"
+  function helpContents(name, commands) {
+     return "<!DOCTYPE html>\n<html>\n  <head>\n  <meta charset=\"utf-8\">\n  <title>" + name + " Help</title>\n  <style type=\"text/css\">\n    body {\n      background: #d3d6d9;\n"
      + " color: #636c75;\n      text-shadow: 0 1px 1px rgba(255, 255, 255, .5);\n      font-family: Helvetica, Arial, sans-serif;\n    }\n    h1 {\n      margin: 8px 0;\n      padding: 0;\n"
      + "    }\n    .commands {\n      font-size: 13px;\n    }\n    p {\n      border-bottom: 1px solid #eee;\n      margin: 6px 0 0 0;\n      padding-bottom: 5px;\n    }\n    p:last-child {\n"
-     + "      border: 0;\n    }\n  </style>\n  </head>\n  <body>\n    <h1>" + name + " Help</h1>\n    <div class=\"commands\">\n      " + commands + "\n    </div>\n  </body>\n</html>";
-
-  getHelpCommands = bot => {
-    var help_commands, bot_name;
-    help_commands = bot.helpCommands();
-    bot_name = bot.alias || bot.name;
+     + "      border: 0;\n    }\n  </style>\n  </head>\n  <body>\n    <h1>" + name + " Help</h1>\n    <div class=\"commands\">\n      " + commands + "\n    </div>\n  </body>\n</html>"
+  }
+  function getHelpCommands(bot) {
+    var help_commands = bot.helpCommands();
     if(hiddenCommandsPattern()) {
       help_commands = help_commands.filter(command => !hiddenCommandsPattern().test(command))
     }
-    help_commands = help_commands.map(command => command.replace(/^(hubot|mubot)/i, bot_name));
+    help_commands = help_commands.map(command => command.replace(/^(hubot|mubot)/i, bot.alias || bot.name));
     return help_commands.sort()
-  };
-
-  hiddenCommandsPattern = () => {
-    var hiddenCommands;
-    hiddenCommands = hiddenCommands = process.env.HUBOT_HELP_HIDDEN_COMMANDS ? hiddenCommands.split(',') : false;
+  }
+  function hiddenCommandsPattern() {
+    var hiddenCommands = process.env.HUBOT_HELP_HIDDEN_COMMANDS;
     if(hiddenCommands) {
-      return new RegExp("^(hubot|mubot) (?:" + hiddenCommands.join('|') + ") - ")
+      return new RegExp("^(hubot|mubot) (?:" + hiddenCommands.replace(/,/g, '|') + ") - ")
     }
-  };
-
+  }
   module.exports = bot => {
     bot.respond(/help(?:\s+(.*))?$/i, msg => {
       var cmds, replyText, filter;
@@ -55,20 +48,15 @@
         if(cmds.length === 0) {
           return msg.send("No available commands match " + filter)
         }
-      }
-      replyText = cmds.join('\n');
-      if(filter) {
+        replyText = cmds.join('\n');
         msg.send(replyText);
       } else {
         let room = msg.message.user.id
         bot.adapter.send({room: msg.message.user.id}, replyText)
-        // bot.send({
-        //   room: (ref = msg.message) != null ? (ref1 = ref.user) != null ? ref1.id : void 0 : void 0
-        // }, replyText);
       }
     });
-    if(process.env.HUBOT_HELP_DISABLE_HTTP == null) {
-      bot.router.get("/" + bot.name + "/help", function(req, res) {
+    if(!process.env.HUBOT_HELP_DISABLE_HTTP) {
+      bot.router.get("/" + bot.name + "/help", (req, res) => {
         var cmds, replyText;
         cmds = renamedHelpCommands(bot).map(cmd => cmd.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'));
         if(req.query.q != null) {
@@ -80,6 +68,5 @@
         res.end(helpContents(bot.name, replyText))
       })
     }
-  };
-
+  }
 }).call(this);
