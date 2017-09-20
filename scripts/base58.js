ec6eda2263c8124ef0568fec6787219e3822e9d6
leathan <1@leathan.xyz>
2017-09-20 18:19:39 -0400

    Refactor

--- scripts/base58.js
+++ scripts/base58.js
@@ -12,78 +12,66 @@
 //
 // Author:
 //   jimeh
-
+//
 (function() {
   var Base58, Base58Builder;
-
-  module.exports = function(robot) {
-    robot.respond(/base58 encode( me)? (.*)/i, function(msg) {
-      var e;
+  module.exports = bot => {
+    bot.respond(/base58 encode(?: me)? (.*)/i, function(msg) {
       try {
-        return msg.send(Base58.encode(msg.match[2]));
-      } catch (error) {
-        e = error;
+        msg.send(Base58.encode(msg.match[1]))
+      } catch (e) {
         if (e.message !== 'Value passed is not an integer.') {
-          throw e;
+          throw e
         }
-        return msg.send("Base58 encoding only works with Integer values.");
+        msg.send("Base58 encoding only works with Integer values.")
       }
     });
-    return robot.respond(/base58 decode( me)? (.*)/i, function(msg) {
-      var e;
+    bot.respond(/base58 decode(?: me)? (.*)/i, function(msg) {
       try {
-        return msg.send(Base58.decode(msg.match[2]));
-      } catch (error) {
-        e = error;
+        msg.send(Base58.decode(msg.match[1]));
+      } catch (e) {
         if (e.message !== 'Value passed is not a valid Base58 string.') {
-          throw e;
+          throw e
         }
-        return msg.send("Not a valid base58 encoded string.");
+        msg.send("Not a valid base58 encoded string.")
       }
-    });
+    })
   };
-
   Base58Builder = (function() {
     function Base58Builder() {
       this.alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
-      this.base = this.alphabet.length;
+      this.base = this.alphabet.length
     }
-
     Base58Builder.prototype.encode = function(num) {
       var mod, str;
-      if (!/^\d+$/.test(num)) {
-        throw new Error('Value passed is not an integer.');
+      if(!/^\d+$/.test(num)) {
+        throw new Error('Value passed is not an integer.')
       }
-      if (typeof num !== 'number') {
-        num = parseInt(num);
+      if(typeof num !== 'number') {
+        num = parseInt(num)
       }
       str = '';
-      while (num >= this.base) {
+      while(num >= this.base) {
         mod = num % this.base;
         str = this.alphabet[mod] + str;
-        num = (num - mod) / this.base;
+        num = (num - mod) / this.base
       }
-      return this.alphabet[num] + str;
+      return this.alphabet[num] + str
     };
-
     Base58Builder.prototype.decode = function(str) {
       var char, char_index, i, index, len, num, ref;
       num = 0;
       ref = str.split("").reverse();
-      for (index = i = 0, len = ref.length; i < len; index = ++i) {
+      for(index = i = 0, len = ref.length; i < len; index = ++i) {
         char = ref[index];
-        if ((char_index = this.alphabet.indexOf(char)) === -1) {
-          throw new Error('Value passed is not a valid Base58 string.');
+        if((char_index = this.alphabet.indexOf(char)) === -1) {
+          throw new Error('Value passed is not a valid Base58 string.')
         }
-        num += char_index * Math.pow(this.base, index);
+        num += char_index * Math.pow(this.base, index)
       }
-      return num;
+      return num
     };
-
-    return Base58Builder;
-
+    return Base58Builder
   })();
-
-  Base58 = new Base58Builder();
-
+  Base58 = new Base58Builder()
 }).call(this);
--- scripts/base64.js
+++ scripts/base64.js
@@ -12,15 +12,14 @@
 //
 // Author:
 //   jimeh
-
+//
 (function() {
-  module.exports = function(robot) {
-    robot.respond(/base64 encode( me)? (.*)/i, function(msg) {
-      return msg.send(new Buffer(msg.match[2]).toString('base64'));
+  module.exports = bot => {
+    bot.respond(/base64 encode(?: me)? (.*)/i, msg => {
+      msg.send(new Buffer(msg.match[1]).toString('base64'))
     });
-    return robot.respond(/base64 decode( me)? (.*)/i, function(msg) {
-      return msg.send(new Buffer(msg.match[2], 'base64').toString('utf8'));
-    });
-  };
-
+    bot.respond(/base64 decode(?: me)? (.*)/i, msg => {
+      return msg.send(new Buffer(msg.match[1], 'base64').toString('utf8'))
+    })
+  }
 }).call(this);
