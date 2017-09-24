ba599efd87892b1dcd0275572ec2a95c688f39c9
leathan <1@leathan.xyz>
2017-09-24 03:47:07 -0400

    moved script

--- scripts/specialtalks.js
+++ scripts/specialtalks.js
@@ -3,16 +3,25 @@
 //
 // Commands:
 //   hubot yell <text> - Yells the text.
+//   hubot hax <text> - Displays text with alternate capitalization.
 //   hubot leet[ me] <text> - returns text in leetspeak.
 //
 // Author:
 //   leathan
 //
-
 (function() {
   module.exports = bot => {
     bot.respond(/yell(?: me)? (.*)$/i, res => res.send(res.match[1].toUpperCase()));
     bot.respond(/leet(?: me)? (.*)/i, res => res.send(require('1337')(res.match[1])));
+    bot.respond(/ha(?:x|cks)(?: me)? (.*)$/i, res => {
+      var result = "", txt = res.match[1];
+      for(let i = 0, l = txt.length;  i < l; ++i) {
+        let char = String.fromCharCode(txt.charCodeAt(i));
+        char = i % 2 ? char.toUpperCase() : char.toLowerCase();
+        result = result.concat(char)
+      }
+      res.send(result)
+    })
     bot.respond(/spam(?: me)? (\d{1,3})? ?(.*)$/i, res => {
       var spamAmount = res.match[1] || 5;
       if(spamAmount > 25) return res.send("Sorry the most I can span is 25 lines.");
