f8a8a84e9c1fd818929aac184ceacb59d3d34905
leathan <1@leathan.xyz>
2017-09-24 03:47:07 -0400

    moved script

--- scripts/code.js
+++ scripts/code.js
@@ -8,8 +8,8 @@
 //   None
 //
 // Commands:
-//   view <file> - Displays contents of file.
-//   \`\`\`//file <data>\`\`\` - Creates file with data
+//   mubot view <file> - Displays contents of file.
+//   mubot create [mode] <file> <contents> - Creates file with with specified contents.
 //
 // Author:
 //   leathan
@@ -18,23 +18,25 @@
 (function() {
   const fs = require('fs');
   module.exports = bot => {
-    bot.hear(/^```#(\w )?(.*?)\n((.|\s)+)```$/, { id: 'create.file' }, msg => {
+    bot.hear(/^create(?:me )? (\w )?(.*?)(?\s|\n)(.|\s)+```$/, { id: 'create.file' }, msg => {
       // $1 = mode, $2 = filename, $3 = content
       if(msg.match[1] === 'a ') {
         fs.appendFile(__dirname + "/" + msg.match[2], "\n" + msg.match[3], err => {
-          msg.send(err || "Appended to ```" + msg.match[2] + "``` Content ```coffeescript\n" + msg.match[3] + "```", msg)
+          msg.send(err || "Appended to ```" + msg.match[2] + "``` Content ```javascript\n" + msg.match[3] + "```", msg)
         })
       } else {
-        fs.writeFile(__dirname + "/" + msg.match[2], msg.match[3], function(err) {
-          msg.send(err || "Created ```" + msg.match[2] + "``` Content ```coffeescript\n" + msg.match[3] + "```", msg)
+        fs.writeFile(__dirname + "/" + msg.match[2], msg.match[3], err => {
+          msg.send(err || "Created ```" + msg.match[2] + "``` Content ```javascript\n" + msg.match[3] + "```", msg)
         })
       }
     });
-    bot.hear(/^view (.+)$/, { id: 'view.file' }, msg => {
-      fs.readFile(__dirname + "/" + msg.match[1], function(err, data) {
+    bot.respond(/^view (.+)$/, { id: 'view.file' }, msg => {
+      var file = /\./.test(data) ? msg.match[1] : msg.match[1] + '.js');
+      fs.readFile(__dirname + "/" + file, (err, data) => {
         if(err) return msg.send(err);
         data = data.toString().replace(/`/g, '\\\`');
-        msg.send("Viewing ```" + msg.match[1] + "```Contents ```coffeescript\n" + data + "```")
+        // If there's no extension add default .js.
+        msg.send("Viewing ```" + msg.match[1] + "```Contents ```javascript\n" + data + "```")
       })
     })
   }
