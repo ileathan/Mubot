cb5e03e63190ff25efb4af3892a895dbc4363025
leathan <1@leathan.xyz>
2017-09-30 16:21:55 -0400

    refactor

--- scripts/http-post-say.js
+++ scripts/http-post-say.js
@@ -21,29 +21,26 @@
 // Author:
 //   insom
 //   luxflux
-
+//
 (function() {
-  module.exports = function(robot) {
-    return robot.router.post("/hubot/say", function(req, res) {
+  module.exports = bot => {
+    bot.router.post("/hubot/say", function(req, res) {
       var body, envelope, message, room;
       body = req.body;
       room = body.room;
       message = body.message;
-      robot.logger.info("Message '" + message + "' received for room " + room);
-      envelope = robot.brain.userForId('broadcast');
+      bot.logger.info("Message '" + message + "' received for room " + room);
+      envelope = bot.brain.userForId('broadcast');
       envelope.user = {};
-      if (room) {
-        envelope.user.room = envelope.room = room;
+      if(room) {
+        envelope.user.room = envelope.room = room
       }
       envelope.user.type = body.type || 'groupchat';
-      if (message) {
-        robot.send(envelope, message);
+      if(message) {
+        bot.send(envelope, message)
       }
-      res.writeHead(200, {
-        'Content-Type': 'text/plain'
-      });
-      return res.end('Thanks\n');
-    });
-  };
-
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Thanks\n')
+    })
+  }
 }).call(this);
