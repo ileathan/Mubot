7e116bc4d1ee11e7cace43bc01cb32acd94c309c
leathan <1@leathan.xyz>
2017-12-09 07:49:49 -0500

    bugfix

---
+++ stratum-proxy/leatProxy.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const config = {
+  host: 'pool.supportxmr.com',
+  port: 3333,
+  key: fs.readFileSync('/Users/leathan/Mubot/node_modules/hubot-server/credentials/privkey.pem'),
+  cert: fs.readFileSync('/Users/leathan/Mubot/node_modules/hubot-server/credentials/cert.pem')
+};
+
+const leatProxy = new (require('leat-stratum-proxy'))(config);
+
