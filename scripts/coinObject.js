f26c4e34a23101120116d690c9f2e982f06ed737
leathan <1@leathan.xyz>
2018-01-01 01:01:26 -0500

    refactor 2 scripts into mubot modules

--- scripts/coinObject.js
+++ scripts/coinObject.js
@@ -4,143 +4,132 @@
 // Commands:
 //   !coins [byname|byprice|byticker|byrank|byvolume]
 //
-
 (function(){
-  const API = "https://api.coinmarketcap.com/v1/ticker?limit=0";
-  const request = require('request');
-  const returnMode = {json: 1, raw: 1, raw: 1, r: 1, name: 2, view: 2, list: 2, names: 2, n: 2};
-  const modes = Object.keys(returnMode);
-  const cmdToCoinKey = {
-    id: 'id',
-    name: 'name',
-    ticker: 'symbol',
-    rank: 'rank',
-    price: 'price_usd',
-    price_btc: 'price_btc',
-    volume: '24h_volume_usd',
-    'market cap': 'market_cap_usd',
-    supply: 'available_supply',
-    'total supply': 'total_supply',
-    'max supply': 'max_supply',
-    'percent 1h': 'percent_change_1h',
-    'percent 1d': 'percent_change_24h',
-    percent: 'percent_change_7d',
-    'max supply': 'max_supply',
-    percent_change_1h: 'percent_change_1h',
-    percent_change_1d: 'percent_change_24h',
-    percent_change_7d: 'percent_change_7d',
-    'last updated': 'last_updated'
-  };
+  // Exports
+  module.exports = bot => {
+    bot.brain.on('loaded', () => {
+      // Export module.
+      Object.assign(bot.mubot, {coins: l});
+    });
+    l.refresh({bot});
+    bot.hear(/^!coins( (load|build|ref(resh)?))? build$/, l.refresh)
+    ;
+  }
+  ;
+  const l = {};
+  l.asObj = {};
+  l.asArray = [];
+  l.utils = {};
 
-  const commands = Object.keys(cmdToCoinKey);
-  //
-  const stringToCmdRe = string => {
+  l.api_endpoint = "https://api.coinmarketcap.com/v1/ticker?limit=0";
+  const http = require('request');
+  l.utils.returnMode = {json: 1, raw: 1, raw: 1, r: 1, name: 2, view: 2, list: 2, names: 2, n: 2};
+
+  l.utils.stringToCmdRe = string => {
     for(let cmd of commands) {
-      if(RegExp(cmdToStringArray(cmd).join('|'), 'i').test(string))
-        return '(' + cmdToStringArray(cmd).join('|') + ')'
+      if(RegExp(l.utils.cmdToStringArray(cmd).join('|'), 'i').test(string))
+        return '(' + l.utils.cmdToStringArray(cmd).join('|') + ')'
       ;
     }
   }
   ;
-  const stringToCmd = string => {
+  l.utils.stringToCmd = string => {
     for(let cmd of commands) {
-      if(RegExp('^('+cmdToStringArray(cmd).join('|')+')$', 'i').test(string))
+      if(RegExp('^('+l.utils.cmdToStringArray(cmd).join('|')+')$', 'i').test(string))
         return cmd
       ;
     }
   }
   ;
-  const cmdToStringArray = cmd => {
-		switch(cmd) {
+  l.utils.cmdToStringArray = cmd => {
+    switch(cmd) {
       case 'name':
-     		return ['byname', 'bn', 'name', 'n']
-     		break;
+        return ['byname', 'bn', 'name', 'n']
+        break;
       case 'ticker':
-     		return ['bysymbol', 's', 'byticker', 'bt', 'ticker', 't']
-     		break;
+        return ['bysymbol', 's', 'byticker', 'bt', 'ticker', 't']
+        break;
       case 'rank':
-     		return ['byrank', 'br', 'rank', 'r']
-     		break;
+        return ['byrank', 'br', 'rank', 'r']
+        break;
       case 'price':
-     		return ['byprice', 'price']
-     		break;
+        return ['byprice', 'price']
+        break;
       case 'percent':
-     		return ['percent', '%', 'per']
-     		break;
+        return ['percent', '%', 'per']
+        break;
       case 'supply':
-     		return ['supply', 'sup']
-     		break;
+        return ['supply', 'sup']
+        break;
       case 'price':
-     		return ['byprice', 'price']
-     		break;
-    	default: return [cmd];
-		}
+        return ['byprice', 'price']
+        break;
+      default: return [cmd];
+    }
   }
   ;
-  var Coins = {};
-  var CoinsArray = [];
-  module.exports = bot => {
-    buildCoins();
-    bot.hear(/^!coins build$/, buildCoins)
-    ;
-    for(let cmd of commands)
-      bot.hear(buildRegex(cmd), getMatches)
-    ;
-  }
-  ;
-  const buildRes = (matches, mode) =>
-      mode === 0 ?
-        matches.length + " matches."
+
+  l.utils.buildRes = (matches, mode) =>
+    mode === 0 ?
+      matches.length + " matches."
+    :
+      mode === 2 ?
+        matches.reduce((a,_)=>a.push(_.symbol) && a,[]).join(', ')
       :
-        mode === 2 ?
-          matches.reduce((a,_)=>a.push(_.symbol) && a,[]).join(', ')
-        :
-          JSON.stringify(matches, null, 2)
+        JSON.stringify(matches, null, 2)
+      //
+    //
   ;
   // const search = (coins, value) => coins.filter(_=>RegExp('^' + value + '$', 'i').test(_[cmdToKey[value]])
-  const getMatches = msg => {
-    var mode = returnMode[msg.match[1]] || 0,
-        cmd = stringToCmd(msg.match[2]),
-        search = msg.match[3].replace(/(?:`((?:\\.|[^`])+)`)/, '$1') || msg.match[3],
-        matches = []
+  l.getMatches = res => {
+    let mode = l.utils.returnMode[res.match[1]] || 0,
+        cmd = l.utils.stringToCmd(res.match[2]),
+        // Transform `\`Hello, World!\`` -> `Hello World`
+        reStr = res.match[3].replace(/`((?:\\.|[^`])+)`/, '$1') || res.match[3],
+        matches = [], re = {}
     ;
-    var re = {};
-    if(search[0] === '/') {
-      re.options = search.split(/\//).pop() || "";
-      re.source = search.slice(1, -re.options.length - 1);
+    if(reStr[0] === '/') {
+      // Transform '/foo/var/flags' -> 'flags'
+      re.flags = reStr.replace(/.*[/](.*?)$/, "$1");
+      re.source = reStr.slice(1, -re.flags.length - 1);
     } else {
-      re.options = 'i';
-      re.source = '^(' + search.trim().split(/[\W]/).join('|') + ')$';
+      re.flags = 'i';
+      re.source = '^(' + reStr.trim().split(/[\W]/).join('|') + ')$';
     }
-    matches = CoinsArray.filter(_=>
-      RegExp(re.source, re.options).test(_[cmdToCoinKey[cmd]])
+    matches = asArray.filter(_=>
+      RegExp(re.source, re.flags).test(_[l.utils.cmdToCoinKey[cmd]])
     )
     ;
-    msg.send(buildRes(matches, mode))
+    res.send(l.utils.buildRes(matches, mode))
     ;
   }
   ;
-  const buildCoins = msg => request(API, (_, __, data) => {
-    var coins = JSON.parse(data)
+  l.refresh = res => http(l.api_endpoint, (_, __, data) => {
+
+    l.asArray = JSON.parse(data)
     ;
-    CoinsArray = coins
+    if(!res.send) {
+      l.commands = Object.keys(l.asArray[0]);
+      ;
+      for(let cmd of l.commands)
+        res.bot.hear(l.utils.buildRegex(cmd), l.getMatches)
+      ;
+    }
+    res.send && res.send(l.asArray.length + " processed.")
     ;
-    msg && msg.send(CoinsArray.length + " processed.")
-    ;
-    for(let coin of CoinsArray) {
+    for(let coin of l.asArray) {
       let keys = Object.keys(coin)
       ;
       for(let key of keys)
-        Coins[key] ? Coins[key].push(coin) : Coins[key] = [coin]
+        l.asObj[key] ? l.asObj[key].push(coin) : l.asObj[key] = [coin]
       ;
     }
     ;
-    // For debugging.
+    return "refreshed"
   })
   ;
-  const buildRegex = cmd => {
-    return new RegExp('^!coins (?:-?(' + modes.join('|') + ') )?-?(' + cmdToStringArray(cmd).join('|') + ') (.+)$', 'i')
-    ;
-  }
+  l.utils.buildRegex = cmd =>
+    RegExp('^!coins (?:-?('+Object.keys(l.utils.returnMode).join('|')+') )?-?('+l.utils.cmdToStringArray(cmd).join('|')+') (.+)$', 'i')
+  ;
 }).call(this)
 ;
--- scripts/mubot-eval.js
+++ scripts/mubot-eval.js
@@ -1,432 +1,132 @@
-// Description:
-//  evaluate commands against live code. (No need to hotreload)
+// Descriptions:
+//  Yet another mubot crypto package
 //
 // Commands:
-//  !always [on/off] - Allows execution of code placed anywhere in text like `bot.name`
-//  \`\`\`'Hello, World!'\`\`\` - Executes the command against the bots live code.
+//   !coins [byname|byprice|byticker|byrank|byvolume]
 //
-//
-const l = {};
-const _eval = require('eval');
-const {inspect} = require('util');
-//const inspect = require('object-inspect');
-
-module.exports = bot => {
-  // Load commands from brain.
-  bot.brain.on('loaded', () => {
-    l.evals = bot.brain.data.evals || (bot.brain.data.evals = {})
-    l.saved = bot.brain.data.savedEvals || (bot.brain.data.savedEvals = {})
-    l.always = bot.brain.data.alwaysEval || (bot.brain.data.alwaysEval = {})
-
-    // Export
-    Object.assign(bot.mubot, l);
-  });
-  // Capture all commands.
-  bot.hear(RegExp('^(?:[!]|(?:[@]?' + (bot.name || bot.alias) + '\s*[:,]?\s*[!]))(.+)', 'i'), l.processMessage)
-  // Configure inspector.
-  bot.respond(/(?:set )?(?:nest level|level|nest)(?: me)?(?: (.+))?/i, msg => {
-    l.setDepth(msg.match[1] || 0);
-    msg.send("Inspects nesting level set to " + depth + ".");
-  })
-  bot.respond(/(?:set )?(?:arr(?:ay)?)?(?: length)?(?: (.+))?/i, msg => {
-    l.maxArrayLength(msg.match[1] || 1);
-    msg.send("Inspects array maxArrayLength set to " + maxArrayLength + ".")
-  })
-  bot.respond(/(?:set )?(?:message length|message|max)(?: (.+))?/i, msg => {
-    l.setMaxMessageLength(msg.match[1] || 1917);
-    msg.send("Inspects max length set to " + maxArrayLength + ".");
-  })
-  // Capture all markdown code.
-  bot.respond(/```[a-z]*\n?((?:.|\n)+)\n?```/i, msg => {
-    l.always[l.msgToUserId(msg)] ||
-      l.realEval(msg)
-    ;
-  });
-  bot.hear(/(?:[^!]|)(?:`((?:\\.|[^`])+)`|```[a-z]*\n?((?:.|\n)+)\n?```)/i, msg => {
-    msg.match[1] = msg.match[1] || msg.match[2];
-    l.always[l.msgToUserId(msg)] &&
-      l.realEval(msg)
-    ;
-  });
-}
-;
-// Persistant
-l.allowed = ['183771581829480448', 'U02JGQLSQ']; // CHANGE THESE TO YOUR ID'S!!
-l.evals = {};
-l.saved = {};
-// Not persistent
-l.always = {};
-l.last_mode = null;
-
-let maxArrayLength = 1;
-let depth = 0;
-let maxMessageLength = 1917; 
-
-l.setMaxMessageLength = _ => maxMessageLength = _ | 0;
-l.setMaxArrayLength = _ => maxArrayLength = _ | 0;
-l.setDepth = _ => depth = _ | 0;
-
-l.preventHacks = msg => {
-  safeBot = {};
-  Object.assign(safeBot, msg.bot);
-  delete safeBot.leat.secure;
-  delete safeBot.cookieToUsername;
-  return safeBot;
-}
-;
-l.realEval = msg => {
-  let cmd = evalCmd = msg.match[1],
-      id = msg.message.user.id,
-      afterCmd = msg.match.input.split('`').pop() || "",
-      // Remove sensitive data from bot.
-      bot = l.preventHacks(msg),
-      opts = [], o = ""
+(function(){
+  // Exports
+  module.exports = bot => {
+    bot.brain.on('loaded', () => {
+      // Export module.
+      Object.assign(bot.mubot, {coins: l});
+    });
+    l.refresh({bot});
+    bot.hear(/^!coins( (load|build|ref(resh)?))? build$/, l.refresh);
+  }
   ;
-  // Set command options.
-  if(l.allowed.includes(id)) {
-     opts = [{bot, msg}, true]
-  } else {
-     let http = require('request');
-     opts = [{http, request: http, req: http}, false]
-  }
-  // Sanitize the command. 
-  if(!/module[.]exports\s*=/.test(cmd)) {
-    !/return .+/.test(cmd) && (evalCmd = 'return ' + evalCmd);
-    evalCmd = 'module.exports=(()=>{' + evalCmd + '})()';
-  }
-  // filename is the second param.
-  o = _eval(evalCmd, msg.bot.name + "_" + msg.message.user.name, ...opts);
+  const l = {};
+  const http = require('request');
+  l.asObj = {};
+  l.asArray = [];
+  l.utils = {};
+ 
+  l.api_endpoint = "https://api.coinmarketcap.com/v1/ticker?limit=0";
 
-  // Reuse opts variable for the formating options.
-  opts = {};
-  if(afterCmd[0] === '{') {
-    try {
-      Object.assign(opts, JSON.parse(afterCmd));
-    } catch(e) {
-      return msg.send("Error parsing JSON.");
-    }
-  } else {
-    [depth = 0, maxArrayLength = 1] = afterCmd.split(/\s*[\D]\s*/);
-    opts = { depth, maxArrayLength };
-  }
+  l.utils.returnMode = {json: 1, raw: 1, raw: 1, r: 1, name: 2, view: 2, list: 2, names: 2, n: 2};
 
-  // Allow 83 chars for res/oLen display.
-  let oLen = 0;
-  try {
-    if(inspect(o) === '[Function]') {
-      oLen = (o + "").length;
-      if(o > maxMessageLength) {
-        oLen = 'Msg trimmed > 2000 ' + oLen + ''
-      }
-    } else {
-      o || (o = 'void 0');
-      o = inspect(o, opts);
-    }
-  } catch(e) {
-    o = inspect(e);
-  }
-
-  o = o.slice(0, maxMessageLength);
-  o && msg.send('# Output [' + oLen + '] ```' + o + '```');
-
-  l.addToLog(cmd, o, id)
-  msg.bot.brain.save();
-}
-;
-
-
-l.processMessage = (msg, dontRun) => {
-  let cmd = msg.match ? msg.match[1] : msg;
-  let match = void 0, res = "";
-  if(match = cmd.match(/```[a-z]*\n?((?:.|\n)+)\n?```/i)) {
-    if(l.always[msg.message.user.id]) {
-      return;
-    }
-    res = 'realEval';
-  }
-  else if(match = cmd.match(/^`((?:\\.|[^`])+)`/i)) {
-    if(l.always[msg.message.user.id]) {
-      return;
-    }
-    res = 'realEval';
-  }
-  else if(match = cmd.match(/^coins/i)) {
-    res = '';
-  }
-  else if(match = cmd.match(/^(?:[!]|last) ?(.*)?/i)) {
-    res = 'runLastCmd';
-  }
-  else if(match = cmd.match(/^(?:length|amount|amnt) ?(.*)?/i)) {
-    res = 'getLengths';
-  }
-  else if(match = cmd.match(/^(?:list|view|l|saved|evals?)(?: logs?)?(?: ([\S]*))?(?: ([\S]*))?(?: ([\S]*))?([^-]+ [^-]+)?/)) {
-    res = 'viewCmds';
-  }
-  else if(match = cmd.match(/^(?:clear|del(?:ete)?) all ?(.*)?/i)) {
-    res = 'deleteAllCmds';
-  }
-  else if(match = cmd.match(/^(?:clear|del(?:ete)?)(?: ([\S]+))?(?: -?(i(?:gnore)?))?(.+)?$/i)) {
-    res = 'deleteCmds';
-  }
-  else if(match = cmd.match(/^fake ```[a-z]*\n?((?:.|\n)+)\n?```/i)) {
-    res = 'fakeEval';
-  }
-  else if(match = cmd.match(/^fake `(\\.|[^`])+`/i)) {
-    res = 'fakeEval';
-  }
-  else if(match = cmd.match(/^(?:save|rec?(:ord)?|preserve|tag) (.+)(?: (.+))?/i)) {
-    res = 'saveCmd';
-  }
-  else if(match = cmd.match(/^(?:set )?always(?: (.*))?/i)) {
-    res = 'setAlways';
-  }
-  if(!res) return;
-  if(!dontRun) {
-    msg.match = match;
-    l[res](msg);
-  }
-  return !!match;
-}
-
-
-
-l.addToLog = function(cmd, res, id) {
- evals[id] || (evals[id] = (evals[id] = {}));
- evals[id][cmd] ? delete evals[id][cmd] && (evals[id][cmd] = res) : evals[id][cmd] = res;
-}
-
-l.msgToUserId = function(msg) {
-  return msg.message.user.id;
-}
-
-l.getLengths = function(msg) {
-  const mode = l.isModeSave(msg.match[1]);
-  const id = l.msgToUserId(msg);
-  const formatLengthReply = mode => {
-    const obj = mode ? saved[id] || {} : evals[id] || {};
-    const cmds = mode ? Object.values(obj) : Object.keys(obj);
-    const amnt = cmds.length;
-    const last = cmds.pop();
-    return amnt + " " + (mode?'saved':'logged') + " eval(s)." + (amnt ? " Last: " + l.formatCmd(last) : "");
-  }
-  let res = "";
-  mode ?
-    res += formatLengthReply(mode)
-  :
-    res += formatLengthReply(1) + ' ' + formatLengthReply(0)
-  ;
-  msg.send(res);
-}
-;
-l.deleteAllCmds = function(msg) {
-  const id = l.msgToUserId(msg);
-  const mode = msg.match[1];
-  if(mode === 'all') {
-    const amntDelSaved = Object.keys(saved[id] || {}).length;
-    const amntDelLog = Object.keys(evals[id] || {}).length;
-    for(let key in evals[id] || {}) delete evals[id][key];
-    for(let key in saved[id] || {}) delete saved[id][key];
-    return msg.send("Deleted " + amntDelLog + " log evals and " + amntDelSaved + " saved evals.");
-  }
-  const obj = l.isModeSave(mode) ? saved[id] || {} : evals[id] || {};
-  const amntDel = Object.keys(obj).length;
-  for(let key in obj) delete obj[key];
-  msg.send("Deleted " + amntDel + " " + (mode ? "saved" : "log") + " evals.");
-}
-;
-
-l.deleteCmds = function(msg) {
-  const id = l.msgToUserId(msg);
-  if(!saved[id] || !evals[id]) {
-    return msg.send("No log found")
-  }
-  var [, mode, ignore, delCmd ] = msg.match;
-  var startAt, endAt, res;
-  if(saved[id][delCmd]) {
-    res = "Deleted " + l.formatCmd(saved[id][delCmd]) + ".";
-    delete saved[id][delCmd];
-  }
-  else if(Object.keys(evals[id])[delCmd]) {
-    res = "Deleted " + l.formatCmd(evals[id][delCmd]) + ".";
-    delete evals[id][delCmd];
-  }
-  else if(/[!]|last/i.test(delCmd)) {
-    let obj = l.isModeSave(mode) ? saved[id] : evals[id];
-    last_mode === 'saved' ?
-      delCmd = Object.values(saved[id]).pop()
-    :
-      delCmd = Object.keys(evals[id]).pop()
-    ;
-    res = "Deleted " + l.formatCmd(obj[delCmd]) + ".";
-    delete obj[delCmd];
-  } else {
-    let keys = Object.keys(obj);
-    delCmd = delCmd.split(/\s*-\s*/);
-
-    if(delCmd.length === 1) {
-      let i = +delCmd[0];
-      i > 0 ? keys = keys.slice(i) : keys.splice(i);
-      for(let key in keys) delete obj[key];
-      res = "Deleted " + keys.length + " evals."
-    }
-    else if(delCmd.length === 2) {
-      let [startAt, endAt] = delCmd;
-
-      startAt > 0 && --startAt
-      startAt > -1 ?
-        endAt || (endAt = keys.length)
-      :
-        endAt = void 0
+  l.utils.stringToCmdRe = string => {
+    for(let cmd of commands) {
+      if(RegExp(l.utils.cmdToStringArray(cmd).join('|'), 'i').test(string))
+        return '(' + l.utils.cmdToStringArray(cmd).join('|') + ')'
       ;
-      ignore ? keys.splice(startAt, endAt) : keys = keys.slice(startAt, endAt);
-      for(let key in keys) delete obj[key];
-      res = "Deleted " + keys.length + " evals."
     }
-    else res = "Could not parse request."
   }
-  msg.bot.brain.save();
-
-  return msg.send(res || "No Command(s) found.")
-}
-;
-
-l.runLastCmd = function(msg) {
-  const id = l.msgToUserId(msg);
-  var mode = msg.match[1];
-  mode === void 0 && (mode = last_mode || 'evals');
-  mode = l.isModeSave(mode)
-  var obj = mode ? saved[id] || {} : evals[id] || {};
-  var last = mode ? Object.values(obj).pop() : Object.keys(obj).pop();
-
-  if(!last)
-    return msg.send("There is no last " + mode + " command.")
   ;
-  msg.match = [, last];
-  l.realEval(msg);
-}
-;
-l.runCmd = function(msg) {
-  const id = l.msgToUserId(msg);
-  const tag = msg.match[1];
-  var cmd = "";
-  if(commands.includes(tag.toLowerCase()))
-    return
-  ;
-  if(!saved[id] || !evals[id]) {
-    return msg.send("No log found")
+  l.utils.stringToCmd = string => {
+    for(let cmd of commands) {
+      if(RegExp('^('+l.utils.cmdToStringArray(cmd).join('|')+')$', 'i').test(string))
+        return cmd
+      ;
+    }
   }
-  cmd = saved[id][tag] ?
-    (last_mode = 'saved') && saved[id][tag]
-  :
-    (last_mode = 'evals') && Object.keys(evals[id])[tag]
   ;
-  if(!cmd)
-    return msg.send("Command not found.")
-  ;
-  msg.match[1] = cmd;
-  l.realEval(msg);
-}
-;
-l.saveCmd = function(msg) {
-  const id = l.msgToUserId(msg);
-  if(!evals[id]) {
-    return msg.send("No log found")
+  l.utils.cmdToStringArray = cmd => {
+    switch(cmd) {
+      case 'name':
+        return ['byname', 'bn', 'name', 'n']
+        break;
+      case 'ticker':
+        return ['bysymbol', 's', 'byticker', 'bt', 'ticker', 't']
+        break;
+      case 'rank':
+        return ['byrank', 'br', 'rank', 'r']
+        break;
+      case 'price':
+        return ['byprice', 'price']
+        break;
+      case 'percent':
+        return ['percent', '%', 'per']
+        break;
+      case 'supply':
+        return ['supply', 'sup']
+        break;
+      case 'price':
+        return ['byprice', 'price']
+        break;
+      default: return [cmd];
+    }
   }
-  var [, cmdIndx, tag ] = msg.match;
-  if(!tag) {
-    tag = cmdIndx;
-    cmdIndx = Object.keys(evals[id]).length - 1;
-  }
-  if(l.isReserved(tag))
-    return msg.send("Cannot save, your name is a reserved command.")
   ;
-  const cmd = Object.keys(evals[id])[cmdIndex];
-  ;
-  if(!cmd)
-    return msg.send("No command found.")
-  ;
-  saved[id][tag] = cmd;
-  msg.bot.brain.save();
-  msg.send("Saved " + l.formatCmd(cmd) + ' as ' + tag + '.');
-}
-;
-l.formatCmd = function(cmd) {
-  if(!cmd) return null;
-  //cmd = cmd.slice(29, -32).replace(/^return\s*/, '');
-  return '`' + cmd + (cmd.length > 20 ? '..' : '') + '`';
-}
-;
-l.isModeSave = function(modeStr) {
-  return /\s*-?(s(aved?)|tag(ged|s)?|recorded)\s*/.test(modeStr);
-}
 
-l.viewCmds = function(msg) {
-  const id = l.msgToUserId(msg);
-  let [, mode, res, ignore, indexes ] = msg.match
-  ;
-  let [startAt, endAt] = (indexes || "").split(/\s*[-]\s*/);
-  mode = l.isModeSave(mode);
-  let commands = res ? Object.values(mode ? saved[id] : evals[id]) : Object.keys(mode ? saved[id] : evals[id])
-  ;
-  if(startAt === '!') {
-    let remove = startAt.slice(1);
-    !mode && (remove = parseInt(remove));
-    let cmd = commands[remove]
-    let i = mode ? commands.indexOf(remove) : remove;
-    return  msg.send("(1) " + i + ': ' + l.formatCmd(cmd));
-  }
-  global.m = msg.match;
-  commands || (commands = []);
-  startAt = startAt > 0 ? --startAt : startAt || 0;
-  startAt > -1 ?
-    endAt = endAt || commands.length
-  :
-    endAt = void 0
-  ;
-  if(startAt > commands.length || endAt > commands.length || startAt > endAt)
-     return msg.send("Your startAt and endAt parameters are invalid.")
-  ;
-  let oldlength = commands.length;
-
-  ignore ? commands.splice(startAt, endAt) : commands = commands.slice(startAt, endAt);
-
-  let length = commands.length;
-
-  var i = startAt > -1 ?
-    length > 17 ? length - 17 + startAt : startAt
-  :
-    oldlength + +startAt - 1
-  ;
-  length > 17 && (commands = commands.slice(-17));
-  msg.send("("+length+") " + commands.map(_=>
-    i++ + ': ' + l.formatCmd(_)
-  ).join(', '))
-}
-;
-
-l.setAlways = function(msg) {
-  let isAlways = !(msg.match[1] || "").match(/off|false|0|no|x/i);
-  let id = l.msgToUserId(msg);
-  isAlways ?
-    l.always[id] ?
-      msg.send("Eval mode already set to always.")
+  l.utils.buildRes = (matches, mode) =>
+    mode === 0 ?
+      matches.length + " matches."
     :
-      (()=>{
-        l.always[id] = 1;
-        msg.bot.brain.save();
-        msg.send("Eval mode set to always.");
-      })()
-    //
-  :
-    l.always[id] ?
-      (()=>{
-        delete l.always[id];
-        msg.send("Eval mode set to trigger only.");
-        msg.bot.brain.save();
-      })()
-    :
-      msg.send("Eval mode already set to trigger only.")
-    //
+      mode === 2 ?
+        matches.reduce((a,_)=>a.push(_.symbol) && a,[]).join(', ')
+      :
+        JSON.stringify(matches, null, 2)
+      //
+    // 
   ;
-}
+  // const search = (coins, value) => coins.filter(_=>RegExp('^' + value + '$', 'i').test(_[cmdToKey[value]])
+  l.getMatches = res => {
+    let mode = l.utils.returnMode[res.match[1]] || 0,
+        cmd = l.utils.stringToCmd(res.match[2]),
+        // Transform `\`Hello, World!\`` -> `Hello World`
+        reStr = res.match[3].replace(/`((?:\\.|[^`])+)`/, '$1') || res.match[3],
+        matches = [], re = {}
+    ;
+    if(reStr[0] === '/') {
+      // Transform '/foo/var/flags' -> 'flags'
+      re.flags = reStr.replace(/.*[/](.*?)$/, "$1");
+      re.source = reStr.slice(1, -re.flags.length - 1);
+    } else {
+      re.flags = 'i';
+      re.source = '^(' + reStr.trim().split(/[\W]/).join('|') + ')$';
+    }
+    matches = asArray.filter(_=>
+      RegExp(re.source, re.flags).test(_[l.cmdToCoinKey[cmd]])
+    )
+    ;
+    res.send(l.utils.buildRes(matches, mode))
+    ;
+  }
+  ;
+  l.refresh = res => http(l.api_endpoint, (_, __, data) => {
+    l.asArray = JSON.parse(data)
+    ;
+    l.commands = Object.keys(l.asArray[0]);
+    ;
+    for(let cmd of l.commands)
+      res.bot.hear(l.utils.buildRegex(cmd), l.getMatches)
+    ;
+    res.send && res.send(l.asArray.length + " processed.")
+    ;
+    for(let coin of l.asArray) {
+      let keys = Object.keys(coin)
+      ;
+      for(let key of keys)
+        l.asObj[key] ? l.asObj[key].push(coin) : l.asObj[key] = [coin]
+      ;
+    }
+    ;
+    return "refreshed"
+  })
+  ;
+  l.utils.buildRegex = cmd =>
+    RegExp('^!coins (?:-?('+Object.keys(l.utils.returnMode).join('|')+') )?-?('+l.utils.cmdToStringArray(cmd).join('|')+') (.+)$', 'i')
+  ;
+}).call(this)
 ;
