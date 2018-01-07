885ce4cb3943cdc6bd10a68a5e32776523f64555
leathan <1@leathan.xyz>
2018-01-07 06:30:26 -0500

    refactor

--- scripts/mubot-commit.js
+++ scripts/mubot-commit.js
@@ -2,8 +2,14 @@
 //   Allow Mubot to commit code to gihubt.
 //
 ;(function(){
+  const path = require('path');
+  const exec = require('child_process').exec;
   const l = {};
-  l.exports = bot => {
+  Object.defineProperties(l, {
+    commit: {value: Commit, enumerable: true},
+  })
+  ;
+  l.commit.exports = bot => {
     // purposely capture commit pointlessly so we can call the command outside
     // of natural context in the sense we can do `mubot.commit(res)` with another
     // res that wont have higher capture groups set.
@@ -11,33 +17,28 @@
     Object.assign(bot.mubot, {commit: l.commit})
   }
   ;
-  l.imports = {
+  l.commit.imports = {
     exec: require('child_process').exec,
     path: require('path')
   }
   ;
   let commit_msg = "Mubot auto commit.",
-      project_dir = l.imports.path.resolve(`${__dirname}/../`);
+      project_dir = path.resolve(`${__dirname}/../`);
      var commit_files = "."
   ;
-  const Commit = res => {
-    l.imports.exec(
+  function Commit(res) {
+    exec(
       `git add "${project_dir}/${res.match[3]||commit_files}"; git commit -m "${res.match[4]||commit_msg}"; git push;`,
-      (_, err, out)=>res.send(out)
+      (err, stderr, stdout)=>res.send(stdout)
     );
   }
-  ;
-  Object.defineProperties(l, {
-    commit: {value: Commit, enumerable: true},
+  Object.defineProperties(l.commit, {
     imports: {enumerable: false},
     exports: {enumerable: false},
-  })
-  ;
-  Object.defineProperties(l.commit, {
     commitMsg: {set(_){commit_msg = _}, get(){return commit_msg}, enumerable: true},
     projectDir: {set(_){project_dir= _}, get(){return project_dir}, enumerable: true},
     commitFiles: {set(_){commit_files = _}, get(){return commit_files}, enumerable: true},
   })
   ;
-  module.exports = l.exports;
+  module.exports = l.commit.exports;
 }).call(this);
