b3473c1a4a81f6b0810629d7a7558a9f1a0f6061
leathan <1@leathan.xyz>
2018-01-06 20:35:19 -0500

    bugfix

--- scripts/mubot-extras.js
+++ scripts/mubot-extras.js
@@ -3,18 +3,21 @@
 //
 const l = {}
 ;
-module.exports = bot => {
-  bot.brain.on('loaded', () => {
-    // Export
-    Object.assign(bot.mubot, {extras: l});
-  });
-  bot.respond(/.*ping(?: me)?/i, l.ping)
-  bot.respond(/.*command count(?: me)?/i, l.commandCount)
+module.exports = l.exports;
+
+l.exports = bot => {
+  bot.brain.on('loaded', () => Object.assign(bot.mubot, {extras: l}));
+  bot.respond(/.*view brain(? (.+))?i/, l.viewBrain);
+  bot.respond(/.*ping(?: me)?/i, l.ping);
+  bot.respond(/.*command count(?: me)?/i, l.commandCount);
 }
 ;
-l.commandCount = res =>
+l.viewVrain = (res = {send: _=>_}) =>
+  bot.mubot.inspect.run(bot.brain.data, msg.match[1])
+;
+l.commandCount =  (res = {send: _=>_}) =>
   res.send("I am aware of " + res.bot.commands.length + " commands.")
 ;
-l.ping = res =>
+l.ping =  (res = {send: _=>_}) =>
   res.send(res.bot.client.pings[0] + 'ms.');
 ;
--- scripts/save.js
+++ scripts/save.js
@@ -2,41 +2,75 @@
 //   Saves brain to disk and removes its auto save and writes to disk on save event.
 //
 // Commands:
-//   mubot save - Dumps brain to ./brain.json.
+//   mubot save brain - Dumps brain to ./brain.json.
+//   mubot write data - Overwrites dumpfile with data (data can be very relaxed json))
 //
 // Author:
 //   leathan
 //
-(function() {
+;(function() {
   //const circularJSON = require('circular-json');
   const fs = require('fs'), Path = require('path');
   const path = Path.join(__dirname, '/../brain.json');
-  const write = data => {
+  const rjson = require("relaxed-json");
+
+
+  l.imports = {fs, path, rjson};
+
+  l.write = (res = {send: _=>_}) => {
+    let data = res.data || res;
+    if(!data) {
+      return res.send("No write data provided.")
+    }
     try {
-      fs.writeFile(path, JSON.stringify(data), 'utf8', _=>0);
+      fs.writeFile(path, rjson.stringify(data), 'utf8', _=>0);
     } catch(e) {
-      console.log("Mubot-save: Error: Saving brain.")
+      bot.logger.debug("Mubot-save: Error: Saving brain.")
     }
     ;
   }
-  module.exports = bot => {
-
-    bot.respond(/save$/i, res => {
-      bot.brain.save();
-      res.send("Database saved to disk.")
-    })
-    bot.brain.setAutoSave && bot.brain.setAutoSave(false);
-    //bot.brain.on("connected", ()=> {
-      try {
-        var data = fs.readFileSync(path, 'utf-8');
-        data && bot.brain.mergeData(JSON.parse(data));
-        bot.brain.emit('loaded');
-      } catch(err) {
-        if(err.code !== 'ENOENT') console.log(err)
-      }
-    //});
+  ;
+  l.save = (res = {send: _=>_}) => {
+    l.write(bot.brain.data);
+    return res.send("Saving brain data.")
+  }
+  ;
+  l.load = (res = {send: _=>_}) => {
+   res.path || (res.path = Path.join(__dirname, '/../brain.json'));
+   try {
+     let data = fs.readFileSync(res.path, 'utf-8');
+     bot.brain.mergeData(JSON.parse(data));
+     bot.brain.emit('loaded');
+    } catch(err) {
+      bot.logger.debug("Mubot-save: Error: Loading brain.");
+    }
+  }
+  ;
+  l.easyWrite = (res = {send: _=>_}) => {
+    data = rjson.parse((res.match||"")[1]);
+    if(!data) {
+      return res.send("No write data provided.");
+    }
+    if(data[0] !== "{") {
+      data = "{" + data + "}";
+    }
+    l.write(rjson.parse(data))
+  }
+  ;
+  l.exports = bot => {
+    bot.respond(/save brain$/i, l.save);
+    bot.respond(/(set|write) brain (.+)/i, l.easyWrite);
+ 
     bot.brain.on('save', write);
     bot.brain.on('close', write);
     bot.brain.on('shutdown', write);
   }
+  ;
+  Object.defineProperties(l, {
+    imports: {enumberable: false},
+    exports: {enumberable: false}
+  })
+  ;
+  module.exports = l.exports
+  ;
 }).call(this);
